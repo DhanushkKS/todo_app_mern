d3d4aa13e1ca6e1215388a4a80300971dbdea406
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2025-01-07 11:50:23 +0530

    redirect / route  to /todo in frondend app when frontend app is loading

--- MyTodo/src/routes/index.tsx
+++ MyTodo/src/routes/index.tsx
@@ -1,9 +1,10 @@
-import { RouteObject } from "react-router-dom";
+import { Navigate, RouteObject } from "react-router-dom";
 
 import { TodoListPage } from "./routeComponents.ts";
 
 export const useRouteObject = (): RouteObject[] => {
   return [
+    { path: "/", element: <Navigate to="/todo" /> },
     {
       path: "/todo",
       element: <TodoListPage />,
